9937ced68a07f4bafbfe8f41ebcda7204d8dd8ba
unknown <karaut0403@gmail.com>
2024-05-04 00:30:17 +0530

    adding frontend files

--- static/script/script.js
+++ static/script/script.js
@@ -1,1 +1,21 @@
-console.log("script file is connected")
+text_area_input = document.getElementById("phrase-input");
+text_area_input.value = ""
+text_area_output = document.getElementById("phrase-output");
+text_area_output.value = ""
+button_paraphrase = document.getElementById("paraphrase");
+
+
+button_paraphrase.addEventListener("click",()=>{
+    data ={
+        "phrase-input":text_area_input.value
+    }
+    axios.post('http://127.0.0.1:8000/getPhrase', data)
+                .then(response => {
+                    text_area_output.value = response.data.value
+                    console.log('Response:', response.data);
+                })
+                .catch(error => {
+                    // Handle error
+                    console.error('Error:', error.response.data);
+                });
+})
