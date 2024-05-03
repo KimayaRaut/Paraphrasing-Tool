125bbccbe98b135f35eb4068344ba69437efe51c
unknown <karaut0403@gmail.com>
2024-05-04 00:31:37 +0530

    adding route to get phrase

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from model.model import *
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route("/getPhrase", methods=['POST'])
+def getPhrase(): 
+    if request.is_json:
+        data_json = request.json
+        paraphraser = getParaphraser(data_json["phrase-input"])
+        return {'value': paraphraser}
+    else:
+        return jsonify({'error': 'Request must contain JSON data'}), 400
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
